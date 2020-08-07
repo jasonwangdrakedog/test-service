c2783d6025d3c095089bb636ae860ee3c8001350
wangyuan <wangyuan@cmss.chinamobile.com>
2020-08-07 10:58:10 +0800

    feat(22): 33
    44
    
    BREAKING CHANGE: 55
    
    Closes 66

--- src/main/java/com/teg/analysis/controller/TaiYueController.java
+++ src/main/java/com/teg/analysis/controller/TaiYueController.java
@@ -2,10 +2,14 @@ package com.teg.analysis.controller;
 
 
 import com.teg.analysis.aop.Log;
+import com.teg.analysis.config.TraceThreadPoolExecutor;
 import com.teg.analysis.model.DTO.WorkOrderDTO;
 import com.teg.analysis.model.REQ.WorkOrderQueryREQ;
 import com.teg.analysis.model.common.Page;
 import com.teg.analysis.service.TaiYueService;
+import com.teg.analysis.service.impl.TaiYueAnalysisServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,10 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 
 @RestController
 @RequestMapping("ty")
 public class TaiYueController {
+    private final static Logger LOGGER = LoggerFactory.getLogger(TaiYueController.class);
+
 
 
     @Resource
@@ -26,7 +33,6 @@ public class TaiYueController {
     @ResponseBody
     @Log(description = "获取工单列表")
     public Page<WorkOrderDTO> listWorkOrder(WorkOrderQueryREQ workOrderQueryREQ) {
-        workOrderQueryREQ.setTest("d");
         List<WorkOrderDTO> list = taiYueService.findWorkOrder(workOrderQueryREQ);
         Page page = Page.success(list);
         page.setPage(2);
